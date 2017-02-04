bac8be13c9859811436d4352a3aeda23e806fb00
simonini <simonini@simonini-Lenovo-ideapad-300-15ISK>
2017-02-04 17:38:23 -0200

    inicial

---
+++ src/main/java/br/com/hackathon/scales/AutoScaleGeneratorApplication.java
@@ -0,0 +1,12 @@
+package br.com.hackathon.scales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoScaleGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoScaleGeneratorApplication.class, args);
+	}
+}
